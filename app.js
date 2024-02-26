f2a1fafc5d7b9bc71848f133e9270d5f21ea9b64
Cipriyo <94758912+Cipriyo@users.noreply.github.com>
2024-02-26 16:22:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,21 +12,15 @@ document.getElementById("search-form").addEventListener("submit", function(event
 			var engine = document.querySelector('input[name="engine"]:checked').value;
 			if (engine === "google") {
 				window.open('https://www.google.com/search?q=' + searchTerm, "_blank");
-				location.reload();
 			} else if (engine === "bing") {
 				window.open('https://www.bing.com/search?q=' + searchTerm, "_blank");
-				location.reload();
 			} else if (engine === "youtube") {
 				window.open('https://www.youtube.com/results?search_query=' + searchTerm, "_blank");
-				location.reload();
 			} else if (engine === "bingvideo") {
 				window.open('https://www.bing.com/videos/search?q=' + searchTerm, "_blank");
-				location.reload();
 			} else if (engine === "ecosia") {
 				window.open('https://www.ecosia.org/search?method=index&q=' + searchTerm, "_blank");
-				location.reload();
 			} else if (engine === "URL") {
 				window.open(searchTerm, "_blank");
-				location.reload();
 			}
 		});
